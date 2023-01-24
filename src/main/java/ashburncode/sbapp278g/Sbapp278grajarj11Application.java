d100711459392d6310460b9affed041d73db7321
ashburndev <ashburndev@gmail.com>
2023-01-24 08:01:54 -0500

    first commit

---
+++ src/main/java/ashburncode/sbapp278g/Sbapp278grajarj11Application.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp278g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbapp278grajarj11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbapp278grajarj11Application.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/sbapp278g/Sbapp278grajarj11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp278g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapp278grajarj11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
